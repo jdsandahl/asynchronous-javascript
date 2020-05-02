ef3c4fd37f3def15cb9b50e5e0d06bf5d3f0afd9
Justin Sandahl <sandahl.j.d@gmail.com>
2020-05-02 15:00:25 +0100

    refactoring of controllers.js, just some code clean-up removed unnecessary curly braces used within the catch statements, this is a result of the error callback function implicitly returning the response, see short-hand versions of arrow functions

--- src/controllers.js
+++ src/controllers.js
@@ -4,17 +4,13 @@ const jokesController = (req, res) =>
   axios
     .get('https://api.icndb.com/jokes')
     .then(response => res.send({ jokes: response.data.value }))
-    .catch(error => {
-      res.status(error.statusCode).send({ error: error.message })
-    });
+    .catch(error => res.status(error.statusCode).send({ error: error.message }));
 
 const randomJokeController = (req, res) =>
   axios
     .get('https://api.icndb.com/jokes/random?exclude=[explicit]')
     .then(response => res.send({ randomJoke: response.data.value }))
-    .catch(error => {
-      res.status(error.statusCode).send({ error: error.message })
-    });
+    .catch(error => res.status(error.statusCode).send({ error: error.message }));
 
 const personalJokeController = (req, res) => {
   const { first, last } = req.params;
@@ -22,9 +18,7 @@ const personalJokeController = (req, res) => {
   axios
     .get(`https://api.icndb.com/jokes/random?firstName=${first}&lastName=${last}&exclude=[explicit]`,)
     .then(response => res.send({ personalJoke: response.data.value }))
-    .catch(error => {
-      res.status(error.statusCode).send({ error: error.message })
-  });
+    .catch(error => res.status(error.statusCode).send({ error: error.message }));
 };
 
 module.exports = {
