fd5cae62d27ae16bb9af4673d9159fee03ae7158
Justin Sandahl <sandahl.j.d@gmail.com>
2020-05-02 14:28:46 +0100

    refactored jokesController to use the promises style to perform its api request

--- src/controllers.js
+++ src/controllers.js
@@ -2,17 +2,13 @@
 const request = require('request');
 const axios = require('axios');
 
-const jokesController = (req, res) => {
-  request('https://api.icndb.com/jokes', (error, jokesApiResponse) => {
-    if (error) {
-      return res.status(error.statusCode).send({ error: error.message });
-    }
-
-    const parsedResponse = JSON.parse(jokesApiResponse.body);
-
-    res.send({ jokes: parsedResponse.value });
-  });
-};
+const jokesController = (req, res) => 
+  axios
+    .get('https://api.icndb.com/jokes')
+    .then(response => res.send({ jokes: response.data.value }))
+    .catch(error => {
+      res.status(error.statusCode).send({ error: error.message })
+    });
 
 const randomJokeController = (req, res) =>
   axios
