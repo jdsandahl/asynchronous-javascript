40287ab3d8af72fe44e918ddfb26e20165da019d
Justin Sandahl <sandahl.j.d@gmail.com>
2020-05-02 16:03:53 +0100

    created an additional seperate folder specific for test data and migrated the mocksData.js file to that folder

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,7 +5,7 @@
 const request = require('supertest');
 const nock = require('nock');
 const app = require('../src/app');
-const { jokesResponse, randomJokeResponse, personalJokeResponse } = require('../src/mockData');
+const { jokesResponse, randomJokeResponse, personalJokeResponse } = require('../testData/mockData');
 
 describe('GET / - Homepage', () => {
   it('should respond with some homepage markup', async () => {
